54ea4aa8dd883d4e6fc0adc864e2b58d0b8493e3
TonyJoseEruppakkatt <tjeruppakkattu@gmail.com>
2025-07-10 00:01:48 +0530

    yes

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from .views import TodoList, TodoDetail, CSVImportView, ExportTodosView
-from .views import CSVImportView
 
 
 urlpatterns = [
     path('todos/', TodoList.as_view()),
     path('todos/<int:pk>/', TodoDetail.as_view()),
-    path('todos/import/', CSVImportView.as_view()),
     path('api/import-csv/', CSVImportView.as_view(), name='import-csv'),
     path('todos/export/<str:format>/', ExportTodosView.as_view()),  # json, csv, raw, sql
     path('api/todos/<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
